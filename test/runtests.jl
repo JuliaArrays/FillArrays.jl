ebb82eeaa3c50517f05822ac110bdb47578b9a2b
Sheehan Olver <solver@mac.com>
2020-05-23 21:16:32 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -196,7 +196,7 @@ end
     @test_throws ArgumentError mut[2, 1] = 9
 
     D = RectDiagonal([1.,2.], (Base.OneTo(3),Base.OneTo(2)))
-    @test stringmime("text/plain", D) == "3×2 RectDiagonal{Float64,Array{Float64,1},Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}:\n 1.0   ⋅ \n  ⋅   2.0\n  ⋅    ⋅ "
+    @test stringmime("text/plain", D) == "3×2 RectDiagonal{Float64,Array{Float64,1},Tuple{Base.OneTo{$Int},Base.OneTo{$Int}}}:\n 1.0   ⋅ \n  ⋅   2.0\n  ⋅    ⋅ "
 end
 
 # Check that all pair-wise combinations of + / - elements of As and Bs yield the correct
