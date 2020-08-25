d357eaccfd4ce1c67774447e7339bce68b8a28ea
Sheehan Olver <solver@mac.com>
2020-08-25 15:42:35 +0100

    Less verbose show for Eye (#118)
    * Less verbose show for Eye
    
    * Add test
    
    * Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1010,10 +1010,11 @@ if VERSION ≥ v"1.5"
     @testset "print" begin
         @test stringmime("text/plain", Zeros(3)) == "3-element Zeros{Float64}"
         @test stringmime("text/plain", Ones(3)) == "3-element Ones{Float64}"
-        @test stringmime("text/plain", Fill(7,2)) == "2-element Fill{Int64}: entries equal to 7"
+        @test stringmime("text/plain", Fill(7,2)) == "2-element Fill{$Int}: entries equal to 7"
         @test stringmime("text/plain", Zeros(3,2)) == "3×2 Zeros{Float64}"
         @test stringmime("text/plain", Ones(3,2)) == "3×2 Ones{Float64}"
-        @test stringmime("text/plain", Fill(7,2,3)) == "2×3 Fill{Int64}: entries equal to 7"
+        @test stringmime("text/plain", Fill(7,2,3)) == "2×3 Fill{$Int}: entries equal to 7"
+        @test stringmime("text/plain", Eye(5)) == "5×5 Eye{Float64}"
     end
 end
 
