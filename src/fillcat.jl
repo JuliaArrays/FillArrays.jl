7bca7093fe7b81d2f4b776a714a1af6136b8f366
DrChainsaw <christian.kyril.skarby@gmail.com>
2021-02-21 23:35:59 +0100

    Fix cat_shape for julia < 1.6

--- src/fillcat.jl
+++ src/fillcat.jl
@@ -9,7 +9,7 @@ function Base.cat_t(::Type{T}, fs::Fill...; dims) where T
     # There might be some cases when it does not get padded which are not considered here
     allvals[] !== zero(T) && sum(catdims) > 1 && return Base._cat_t(dims, T, fs...)
 
-    shape = Base.cat_shape(catdims, map(Base.cat_size, fs)::Tuple{Vararg{Union{Int,Dims}}})::Dims
+    shape = cat_shape_fill(catdims, fs)
     return Fill(convert(T, fs[1].value), shape)
 end
 
@@ -19,7 +19,7 @@ Base.hcat(vs::Fill...) = cat(vs...;dims=Val(2))
 
 function Base.cat_t(::Type{T}, fs::Zeros...; dims) where T
     catdims = Base.dims2cat(dims)
-    shape = Base.cat_shape(catdims, map(Base.cat_size, fs)::Tuple{Vararg{Union{Int,Dims}}})::Dims
+    shape = cat_shape_fill(catdims, fs)
     return Zeros{T}(shape)
 end
 
@@ -34,10 +34,16 @@ function Base.cat_t(::Type{T}, fs::Ones...; dims) where T
     # There might be some cases when it does not get padded which are not considered here
     sum(catdims) > 1 && return Base._cat_t(dims, T, fs...)
 
-    shape = Base.cat_shape(catdims, map(Base.cat_size, fs)::Tuple{Vararg{Union{Int,Dims}}})::Dims
+    shape = cat_shape_fill(catdims, fs)
     return Ones{T}(shape)
 end
 
 Base.vcat(vs::Ones...) = cat(vs...;dims=Val(1))
 Base.hcat(vs::Ones...) = cat(vs...;dims=Val(2))
 
+
+if VERSION < v"1.6-"
+    cat_shape_fill(catdims, fs) = Base.cat_shape(catdims, (), map(Base.cat_size, fs)...)
+else
+    cat_shape_fill(catdims, fs) = Base.cat_shape(catdims, map(Base.cat_size, fs)::Tuple{Vararg{Union{Int,Dims}}})::Dims
+end
