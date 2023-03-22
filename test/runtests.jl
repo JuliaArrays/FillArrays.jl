dc8aa6e449773e9640979f4d4e4b3d95ee5e4eb9
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2023-03-22 17:46:35 +0000

    Fix #224 (#225)
    * fix #224
    
    * bump version

--- test/runtests.jl
+++ test/runtests.jl
@@ -308,7 +308,7 @@ as_array(x::AbstractArray) = Array(x)
 as_array(x::UniformScaling) = x
 function test_addition_and_subtraction(As, Bs, Tout::Type)
     for A in As, B in Bs
-        @testset "$A ± $B" begin
+        @testset "$(typeof(A)) ± $(typeof(B))" begin
             @test A + B isa Tout{promote_type(eltype(A), eltype(B))}
             @test as_array(A + B) == as_array(A) + as_array(B)
 
@@ -326,7 +326,7 @@ end
 
 # Check that all permutations of + / - throw a `DimensionMismatch` exception.
 function test_addition_and_subtraction_dim_mismatch(a, b)
-    @testset "$a ± $b" begin
+    @testset "$(typeof(a)) ± $(typeof(b))" begin
         @test_throws DimensionMismatch a + b
         @test_throws DimensionMismatch a - b
         @test_throws DimensionMismatch b + a
@@ -375,6 +375,11 @@ end
     # FillArray + StaticArray should not have ambiguities
     A_svec, B_svec = SVector{5}(rand(5)), SVector(1, 2, 3, 4, 5)
     test_addition_and_subtraction((A_fill, B_fill, Zeros(5)), (A_svec, B_svec), SVector{5})
+
+    # Issue #224
+    A_matmat, B_matmat = Fill(rand(3,3),5), [rand(3,3) for n=1:5]
+    test_addition_and_subtraction((A_matmat,), (A_matmat,), Fill)
+    test_addition_and_subtraction((B_matmat,), (A_matmat,), Vector)
 
     # Optimizations for Zeros and RectOrDiagonal{<:Any, <:AbstractFill}
     As_special_square = (
