4a2018ba191c5d7e68725f91cc19483ab673ed86
Sheehan Olver <solver@mac.com>
2023-03-28 14:56:15 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -634,7 +634,7 @@ end
     @test sum(Fill(3,10)) ≡ 30
     @test reduce(+, Fill(3,10)) ≡ 30
     @test sum(x -> x + 1, Fill(3,10)) ≡ 40
-    @test cumsum(Fill(3,10)) ≡ 3:3:30
+    @test cumsum(Fill(3,10)) ≡ StepRangeLen(3.0,3,10)
 
     @test sum(Ones(10)) ≡ 10.0
     @test sum(x -> x + 1, Ones(10)) ≡ 20.0
