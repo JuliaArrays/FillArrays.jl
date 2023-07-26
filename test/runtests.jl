ecf33318082d9106e7864c4683e57464bdf4a11d
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-07-26 22:34:34 +0530

    Don't test for Project.toml formatting on v1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,11 @@ import FillArrays: AbstractFill, RectDiagonal, SquareEye
 
 using Aqua
 @testset "Project quality" begin
-    Aqua.test_all(FillArrays, ambiguities=false)
+    Aqua.test_all(FillArrays, ambiguities=false,
+        # only test formatting on VERSION >= v1.7
+        # https://github.com/JuliaTesting/Aqua.jl/issues/105#issuecomment-1551405866
+        project_toml_formatting = VERSION >= v"1.7",
+    )
 end
 
 include("infinitearrays.jl")
