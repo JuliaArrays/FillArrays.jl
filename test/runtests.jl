71b503bd71281473907f9723b2fcd33ce229bc9e
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-08-22 22:58:30 +0530

    Type-inference improvement in reduce over Ones

--- test/runtests.jl
+++ test/runtests.jl
@@ -825,6 +825,8 @@ end
     @test diff(Ones{Float64}(10)) ≡ Zeros{Float64}(9)
     @test_throws UndefKeywordError cumsum(Fill(1,1,5))
 
+    @test @inferred(sum([Ones(4)])) ≡ Fill(1.0, 4)
+
     @testset "infinite arrays" begin
         r = InfiniteArrays.OneToInf()
         A = Ones{Int}((r,))
