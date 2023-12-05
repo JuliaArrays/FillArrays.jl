a595e925397c2b65aecc847153bd9b0867adc1f2
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-12-05 23:34:50 +0530

    Change 7. to 7.0 in Fill docstring

--- src/FillArrays.jl
+++ src/FillArrays.jl
@@ -96,7 +96,7 @@ Typically created by `Fill` or `Zeros` or `Ones`
 julia> Fill(7, (2,3))
 2×3 Fill{Int64}, with entries equal to 7
 
-julia> Fill{Float64, 1, Tuple{UnitRange{Int64}}}(7., (1:2,))
+julia> Fill{Float64, 1, Tuple{UnitRange{Int64}}}(7.0, (1:2,))
 2-element Fill{Float64, 1, Tuple{UnitRange{Int64}}} with indices 1:2, with entries equal to 7.0
 ```
 """
