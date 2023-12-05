5b5a1cbcf2e205fc7028ecac6294f5a1e6570e11
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-12-05 14:02:06 +0530

    Update inference tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1395,13 +1395,10 @@ end
     @test @inferred(permutedims(Zeros(2,4,5), [3,2,1])) ≡ Zeros(5,4,2)
     @test @inferred(permutedims(Fill(2.0,2,4,5), [3,2,1])) ≡ Fill(2.0,5,4,2)
 
-    # test for inference only if aggressive constant propagation is available
-    H = if VERSION >= v"1.8"
-        @inferred(permutedims(Fill(2, (SOneTo(2), SOneTo(3)))))
-    else
-        permutedims(Fill(2, (SOneTo(2), SOneTo(3))))
-    end
+    H = @inferred(permutedims(Fill(2, (SOneTo(2), SOneTo(3)))))
     @test H === Fill(2, (SOneTo(3), SOneTo(2)))
+
+    # test for inference only if aggressive constant propagation is available
     F = Fill(2, (SOneTo(2), SOneTo(3), SOneTo(1)))
     H = if VERSION >= v"1.8"
         @inferred((F -> permutedims(F, (3,1,2)))(F))
