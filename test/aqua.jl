43f4ced7709550531a045a5b7c40d9db7d64789f
Sheehan Olver <solver@mac.com>
2026-07-02 22:13:51 +0100

    allow ambiguities in older versions

--- test/aqua.jl
+++ test/aqua.jl
@@ -5,6 +5,7 @@ downstream_test = "--downstream_integration_test" in ARGS
 @testset "Project quality" begin
     Aqua.test_all(FillArrays;
         # https://github.com/JuliaArrays/FillArrays.jl/issues/105#issuecomment-1582516319
+        ambiguities=(; broken=(VERSION < v"1.12")),
         stale_deps = !downstream_test,
     )
 end
